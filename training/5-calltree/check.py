a697f15cd2e80131a33a14652f33247ff85f6f00
Steven Wirsz <swirsz@gmail.com>
2023-08-05 10:50:39 -0700

    Update check.py

--- training/5-calltree/check.py
+++ training/5-calltree/check.py
@@ -2,12 +2,11 @@
 import json
 import sys
 
-print("###\n### Welcome to "+sys.argv[0]+"!\n###\n")
+print("###\n### Welcome to training!\n###\n")
 print("In this challenge, you have a working fuzz driver that improve code coverage")
 print("by calling functions higher in the call tree. Examine the source code of the")
 print("current fuzz driver and the source code of the project to find 2 different")
-print("function that can be swapped for similar functions.\n")
-print("Note: Fuzzing is non-deterministic. You may need to run /challenge/loc again.\n")
+print("functions that can be swapped for similar functions.\n")
 
 summary_file = "/out/fuzzer_stats/newfuzzer.json"
 returncode = 0
