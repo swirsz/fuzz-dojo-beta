cf1a0008764874ab73f683a4b2154865dcfdc469
Steven Wirsz <swirsz@gmail.com>
2023-08-05 09:21:00 -0700

    0

---
+++ training/5-calltree/check.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import json
+import sys
+
+print("###\n### Welcome to "+sys.argv[0]+"!\n###\n")
+print("In this challenge, you have a working fuzz driver that improve code coverage")
+print("by calling functions higher in the call tree. Examine the source code of the")
+print("current fuzz driver and the source code of the project to find 2 different")
+print("function that can be swapped for similar functions.\n")
+print("Note: Fuzzing is non-deterministic. You may need to run /challenge/loc again.\n")
+
+summary_file = "/out/fuzzer_stats/newfuzzer.json"
+try:
+    with open(summary_file, "r") as fj:
+        content = json.load(fj)
+        for dd in content['data']:
+            filenames = dd.get('files')
+            for files in filenames:
+                if files.get('filename') == "/src/bzip2/bzlib.c":
+                    func = files['summary']['branches']['covered']
+                    print("{0} functions reached. ".format(str(func)))
+                    if func < 144:
+                        print("Additional functions could be called higher in the calltree.\n")
+                    else:
+                        print("Congratulations")
+                        exit(int(func))
+except:
+    print("Missing "+summary_file)
+    print("\nPlease run /challenge/loc first\n")
+    exit(-1)
