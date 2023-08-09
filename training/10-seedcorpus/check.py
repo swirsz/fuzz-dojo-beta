8490d508f06b5ea7da7f2fe67a013a5415563f5b
Steven Wirsz <swirsz@gmail.com>
2023-08-08 20:35:13 -0700

    0

--- training/10-seedcorpus/check.py
+++ training/10-seedcorpus/check.py
@@ -2,12 +2,6 @@
 import json
 import sys
 
-print("###\n### Welcome to training!\n###\n")
-print("In this challenge, you have a working fuzz driver that improve code coverage")
-print("by calling functions higher in the call tree. Examine the source code of the")
-print("current fuzz driver and the source code of the project to find 2 different")
-print("functions that can be swapped for similar functions.\n")
-
 summary_file = "/out/fuzzer_stats/bzip2_decompress_target.json"
 returncode = 0
 try:
