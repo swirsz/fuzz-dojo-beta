2aa25a3cb8e141a616b0b2889a5eb8a82bcc95d7
Steven Wirsz <swirsz@gmail.com>
2025-04-09 20:28:12 -0700

    Update check.py

--- training/5-calltree/check.py
+++ training/5-calltree/check.py
@@ -19,7 +19,8 @@ print("than those currently implemented in training-calltree/newfuzzer.c\n")
 
 summary_file = "/out/fuzzer_stats/newfuzzer.json"
 returncode = 0
-target = 19=
+target = 19
+
 try:
     with open(summary_file, "r") as fj:
         content = json.load(fj)
