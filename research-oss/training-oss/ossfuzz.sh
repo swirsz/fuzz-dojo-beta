874b9786bebb4034a5d7a2c8b7416a58cae9f4bc
Steven Wirsz <swirsz@gmail.com>
2025-06-12 09:33:33 -0700

    0

--- research-oss/training-oss/ossfuzz.sh
+++ research-oss/training-oss/ossfuzz.sh
@@ -44,6 +44,7 @@ helper() {
     local command="$1"
     local project_name="$2"
     local fuzzer_name="$3"
+    
     local engine="${4:-$DEFAULT_ENGINE}"
     local sanitizer="${5:-$DEFAULT_SANITIZER}"
     local architecture="${6:-$DEFAULT_ARCHITECTURE}"
@@ -54,6 +55,7 @@ helper() {
     log_info "command: $command"
     log_info "project_name: $project_name"
     log_info "fuzzer_name: $fuzzer_name"
+    
     log_info "engine: $engine"
     log_info "sanitizer: $sanitizer"
     log_info "architecture: $architecture"
@@ -61,17 +63,57 @@ helper() {
     log_info "fuzzer_args: $fuzzer_args"
 
     if [[ "$command" == "build_image" ]]; then
-        log_info
+        log_info "Build Image: $project_name"
+        /challenge/build
     elif [[ "$command" == "build_fuzzers" ]]; then
-        log_info
+        log_info "Build Fuzzers: $project_name"
+        /challenge/build
     elif [[ "$command" == "check_build" ]]; then
-        log_info
+        log_info "Check Build: $project_name"
+        /challenge/build
     elif [[ "$command" == "run_fuzzer" ]]; then
-        log_info
+        log_info "Run Fuzzer: $project_name  options $@"
+        run_fuzzer $fuzzer_name $@
     elif [[ "$command" == "coverage" ]]; then
-        log_info
+        log_info "Coverage: $project_name"
+        rm -rf /corpus/$fuzzer_name
+        mv /tmp/$fuzzer_name_corpus /corpus/$fuzzer_name
+        
+        coverage
+        
+        cp -r /out/report_target/$fuzzer_name /out/report/$fuzzer_name
     elif [[ "$command" == "introspector" ]]; then
-        log_info
+        log_info "Run Fuzzer: $project_name"
+        /challenge/build introspector
+                
+        DRIVERS=(/src/inspector/*.covreport)
+
+        rm -rf /out/introspector-report
+        mkdir -p /out/introspector-report
+        mv /out/inspector /out/introspector-report/inspector
+        mv /out/report /out/introspector-report/inspector/covreport
+
+        # fix delays caused by internet references to unpkg.com & googletagmanager.com
+        patch -tuN /out/introspector-report/inspector/fuzz_report.html -i /usr/local/sbin/fuzz_report.patch -r -
+
+        for f in /out/introspector-report/inspector/calltree_view*.html ; do patch -tuN "$f" -i /usr/local/sbin/calltree_view.patch -r -; done
+        for f in /out/report_target/*; do mv $f /out/introspector-report/inspector/covreport/$(basename $f); done
+
+        zip -y /home/hacker/.log/$(TZ=US/Arizona date +"%F--%H-%M-%S")-fi /out/introspector-report/inspector/fuzz_report.html
+        echo
+        echo Workspace:
+        echo  Coverage: https://pwn.college/workspace/code/proxy/8008/covreport
+        echo  Fuzz-Introspector: https://pwn.college/workspace/code/proxy/8008/fuzz_report.html
+        echo
+        echo Virtual Desktop under Applications/Web Browser:
+        echo  Coverage: http://127.0.0.1:8008/covreport
+        echo  Fuzz-Introspector: http://127.0.0.1:8008/fuzz_report.html
+        echo
+        echo If you encounter errors, run /challenge/fuzz-introspector under shell or Workspace first, then view report on Desktop/Web Browser
+        echo
+        echo \'fi-report\' to restart web server only
+        echo 
+        python3 -m http.server 8008 --directory /out/introspector-report/inspector/
     else
         log_error Syntax:  helper {command} {project name} {options}
     fi
